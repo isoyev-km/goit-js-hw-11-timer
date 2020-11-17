58896b85f8cc0bb6ff1af82aa031fc4f98b2ce3f
Kamil <60015567+isoyev-km@users.noreply.github.com>
2020-11-17 22:23:22 +0200

    fixed date format

--- timer.js
+++ timer.js
@@ -1,3 +1,5 @@
+// Лучше разделить функцию updateTimer() на две. Отдельно математические вычисления и отрисовку интерфейса.
+
 const refs = {
   seconds: document.querySelector(`span[data-value="secs"]`),
   minutes: document.querySelector(`span[data-value="minutes"]`),
@@ -19,10 +21,16 @@ const timer = {
 timer.start();
 
 function updateTimer(time) {
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
   refs.timer.textContent = `Time left: ${days} days ${hours} hours ${mins} mins ${secs} seconds`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, "0");
 }
